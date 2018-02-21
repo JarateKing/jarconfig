b8d92ad8334b7c2db2c740165635912abecbfe54
JarateKing <thecoolestguy@coolsite.net>
2018-02-21 09:48:06 -0400

    made update preserve custom.cfg

--- update_from_github.bat
+++ update_from_github.bat
@@ -6,6 +6,8 @@ SET "current_folder=%cd%"
 SET "repo_name=jarconfig-master"
 TITLE Downloading Files from Github
 
+:: backup files to preserve
+move "%current_folder%\cfg\custom.cfg" "%current_folder%"
 :: download zip from github
 dev\wget.exe https://github.com/JarateKing/jarconfig/archive/master.zip --no-check-certificate
 :: extract zip
@@ -16,3 +18,5 @@ for /F %%A in ("%repo_name%\*") do move /Y "%%~A" "%current_folder%"
 :: remove folders and files used in the process
 rmdir /S /Q %repo_name%
 del master
+:: return preserved files
+move "%current_folder%\custom.cfg" "%current_folder%\cfg\"
