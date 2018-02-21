68ffc6b3d136b9cd2a9ed0e4a872548a2f9208a4
JarateKing <scmacintosh@upei.ca>
2018-02-21 10:39:40 -0400

    added update_from_github for linux

---
+++ update_from_github.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ 
+# backup files to preserve
+mv "./cfg/custom.cfg" "./custom.cfg"
+# download zip from github
+wget https://github.com/JarateKing/jarconfig/archive/master.zip --no-check-certificate
+# extract zip
+unzip master.zip
+# move folders and files out of jarconfig-master
+cp -rf "./jarconfig-master/." "."
+# remove folders and files used in the process
+rm -rf "./jarconfig-master"
+rm -rf "./master.zip" 
+# return preserved files
+mv "./custom.cfg" "./cfg/custom.cfg"
