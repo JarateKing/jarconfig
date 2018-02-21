6be8d6c1ec21ae6bac91cb194a1b8c4a6dadfb73
JarateKing <thecoolestguy@coolsite.net>
2018-02-21 09:51:37 -0400

    added reset custom batch script

---
+++ reset_custom.bat
@@ -0,0 +1,17 @@
+:: Originally made by Rhapsody
+:: Modified by JarateKing
+
+@echo off
+SET "current_folder=%cd%"
+SET "repo_name=jarconfig-master"
+TITLE Downloading Files from Github
+
+:: download zip from github
+dev\wget.exe https://github.com/JarateKing/jarconfig/archive/master.zip --no-check-certificate
+:: extract zip
+dev\7z.exe x master -y
+:: move folders and files out of jarconfig-master
+move "jarconfig-master\cfg\custom.cfg" "cfg"
+:: remove folders and files used in the process
+rmdir /S /Q %repo_name%
+del master
