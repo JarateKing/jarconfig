2aeb58fdcc0ef3f37c60c10baa3a6a4a741fb00a
JarateKing <scmacintosh@upei.ca>
2018-02-27 09:16:31 -0400

    added comments to generate nonworkshop

--- dev/mtp/generate_nonworkshop.py
+++ dev/mtp/generate_nonworkshop.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python
 
+# finds workshop maps in a file and appends their non-workshop versions
+# created by JarateKing
+
 import re
 
 maplist = []
 
+# get list of workshop maps
 f = open("raw.txt","r")
 for line in f:
 	if bool(re.search('workshop\/', line)):
 		maplist.append(line)
 f.close()
 
+# create their non-workshop versions
 for line in maplist:
+	# remove "workshop/"
 	newline = re.sub('workshop\/', '', line)
+	# remove ".ugc12345." of any length
 	newline = re.sub('\.(.*?)\.', '.', newline)
-	newline = re.sub('\.\.', '.', newline)
+	# append to file
 	f = open("raw.txt", "a")
 	f.write(newline)
 	f.close()
