e07bc9211ef7f8c3d01521e19695af028d2e79a5
JarateKing <scmacintosh@upei.ca>
2018-03-05 09:04:46 -0400

    made update_from_github.sh preserve class cfg's

--- update_from_github.sh
+++ update_from_github.sh
@@ -5,6 +5,15 @@
 
 # backup files to preserve
 mv "./cfg/custom.cfg" "./custom.cfg"
+mv "./cfg/scout.cfg" "./scout.cfg"
+mv "./cfg/soldier.cfg" "./soldier.cfg"
+mv "./cfg/pyro.cfg" "./pyro.cfg"
+mv "./cfg/demoman.cfg" "./demoman.cfg"
+mv "./cfg/heavyweapons.cfg" "./heavyweapons.cfg"
+mv "./cfg/engineer.cfg" "./engineer.cfg"
+mv "./cfg/medic.cfg" "./medic.cfg"
+mv "./cfg/sniper.cfg" "./sniper.cfg"
+mv "./cfg/spy.cfg" "./spy.cfg"
 # download zip from github
 wget https://github.com/JarateKing/jarconfig/archive/master.zip --no-check-certificate
 # extract zip
@@ -16,3 +25,12 @@ rm -rf "./jarconfig-master"
 rm -rf "./master.zip" 
 # return preserved files
 mv "./custom.cfg" "./cfg/custom.cfg"
+mv "./scout.cfg" "./cfg/scout.cfg"
+mv "./soldier.cfg" "./cfg/soldier.cfg"
+mv "./pyro.cfg" "./cfg/pyro.cfg"
+mv "./demoman.cfg" "./cfg/demoman.cfg"
+mv "./heavyweapons.cfg" "./cfg/heavyweapons.cfg"
+mv "./engineer.cfg" "./cfg/engineer.cfg"
+mv "./medic.cfg" "./cfg/medic.cfg"
+mv "./sniper.cfg" "./cfg/sniper.cfg"
+mv "./spy.cfg" "./cfg/spy.cfg"
