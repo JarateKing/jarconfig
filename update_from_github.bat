dcaf9084fb40c4ffa68ff837a022de2be374d786
JarateKing <scmacintosh@upei.ca>
2018-03-05 09:07:15 -0400

    made update_from_github.bat preserve class cfg's

--- update_from_github.bat
+++ update_from_github.bat
@@ -8,6 +8,15 @@ TITLE Downloading Files from Github
 
 :: backup files to preserve
 move "%current_folder%\cfg\custom.cfg" "%current_folder%"
+move "%current_folder%\cfg\scout.cfg" "%current_folder%"
+move "%current_folder%\cfg\soldier.cfg" "%current_folder%"
+move "%current_folder%\cfg\pyro.cfg" "%current_folder%"
+move "%current_folder%\cfg\demoman.cfg" "%current_folder%"
+move "%current_folder%\cfg\heavyweapons.cfg" "%current_folder%"
+move "%current_folder%\cfg\pyro.cfg" "%current_folder%"
+move "%current_folder%\cfg\medic.cfg" "%current_folder%"
+move "%current_folder%\cfg\sniper.cfg" "%current_folder%"
+move "%current_folder%\cfg\spy.cfg" "%current_folder%"
 :: download zip from github
 dev\wget.exe https://github.com/JarateKing/jarconfig/archive/master.zip --no-check-certificate
 :: extract zip
@@ -20,3 +29,13 @@ rmdir /S /Q %repo_name%
 del master
 :: return preserved files
 move "%current_folder%\custom.cfg" "%current_folder%\cfg\"
+move "%current_folder%\custom.cfg" "%current_folder%\cfg\"
+move "%current_folder%\scout.cfg" "%current_folder%\cfg\"
+move "%current_folder%\soldier.cfg" "%current_folder%\cfg\"
+move "%current_folder%\pyro.cfg" "%current_folder%\cfg\"
+move "%current_folder%\demoman.cfg" "%current_folder%\cfg\"
+move "%current_folder%\heavyweapons.cfg" "%current_folder%\cfg\"
+move "%current_folder%\pyro.cfg" "%current_folder%\cfg\"
+move "%current_folder%\medic.cfg" "%current_folder%\cfg\"
+move "%current_folder%\sniper.cfg" "%current_folder%\cfg\"
+move "%current_folder%\spy.cfg" "%current_folder%\cfg\"
