cfbc2a3100645d30168d04bd3adc33c6406b17e9
JarateKing <scmacintosh@upei.ca>
2018-03-27 13:13:34 -0300

    added comments to commit.sh

--- dev/mtp/commit.sh
+++ dev/mtp/commit.sh
@@ -3,7 +3,10 @@
 # generates mtp and commits map scans to github
 # created by JarateKing
 
+# create map
 ./generate_mtp.sh
+# add files to new commit
 git add raw.txt
 git add ../../cfg/mtp.cfg
+# commit changes
 git commit -m "added maps from scan"
