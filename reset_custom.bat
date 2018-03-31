e5b6833c6f3fd3fbb64f175020a0a7ce06fa78d0
JarateKing <scmacintosh@upei.ca>
2018-03-31 00:06:23 -0300

    added compatibility layer files to reset_custom

--- reset_custom.bat
+++ reset_custom.bat
@@ -21,6 +21,15 @@ move "jarconfig-master\cfg\engineer.cfg" "cfg"
 move "jarconfig-master\cfg\medic.cfg" "cfg"
 move "jarconfig-master\cfg\sniper.cfg" "cfg"
 move "jarconfig-master\cfg\spy.cfg" "cfg"
+move "jarconfig-master\cfg\scout_c.cfg" "cfg"
+move "jarconfig-master\cfg\soldier_c.cfg" "cfg"
+move "jarconfig-master\cfg\pyro_c.cfg" "cfg"
+move "jarconfig-master\cfg\demoman_c.cfg" "cfg"
+move "jarconfig-master\cfg\heavyweapons_c.cfg" "cfg"
+move "jarconfig-master\cfg\engineer_c.cfg" "cfg"
+move "jarconfig-master\cfg\medic_c.cfg" "cfg"
+move "jarconfig-master\cfg\sniper_c.cfg" "cfg"
+move "jarconfig-master\cfg\spy_c.cfg" "cfg"
 :: remove folders and files used in the process
 rmdir /S /Q %repo_name%
 del master
