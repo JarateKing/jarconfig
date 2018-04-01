635a3bf9b72711fba286431d4803401b9e4f49ff
JarateKing <scmacintosh@upei.ca>
2018-04-01 17:15:40 -0300

    added comments to count_changes.py

--- dev/mtp/count_changes.py
+++ dev/mtp/count_changes.py
@@ -7,13 +7,18 @@ total = 0
 f = open("raw.txt","r")
 for line in f:
     total = total + 1
+
+    # count if it's past the last alphabetical entry
     if start:
         count = count + 1
+
+    # if it finds the last alphabetical entry, start counting
     if line == '"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz.bsp" "1"\n':
         start = True
 f.close()
 
-print('total lines: ' + str(total))
-print('total commited lines: ' + str(total - count))
-print('changes to raw: ' + str(count))
+# print metrics
+print('total maps: ' + str(total))
+print('total commited maps: ' + str(total - count))
+print('changes to raw maps: ' + str(count))
 print('changes after commit: ' + str(count * 2))
